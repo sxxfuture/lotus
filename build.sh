f4a8ec9f6c9d4f643812ece98aaf70357d93114d
yanggaofeng <yanggaofeng@sxxfuture.com>
2022-01-07 01:19:59 +0000

    fix build.sh bug: $ should be \$

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ export ALL_PROXY=socks5://10.0.0.5:1080
 export HTTP_PROXY=http://10.0.0.5:3128
 export HTTPS_PROXY=http://10.0.0.5:3128
 export NO_PROXY="10.0.0.0/8,172.6.0.0/12,192.168.0.0/16,127.0.0.0/8,localhost,lan,local,sxx.zone,sxxfuture.com,sxxfuture.net"
-export all_proxy=$ALL_PROXY
-export http_proxy=$HTTP_PROXY
-export https_proxy=$HTTPS_PROXY
-export no_proxy=$NO_PROXY
+export all_proxy=\$ALL_PROXY
+export http_proxy=\$HTTP_PROXY
+export https_proxy=\$HTTPS_PROXY
+export no_proxy=\$NO_PROXY
 EOF
     cat>noproxy<<EOF
 unset ALL_PROXY all_proxy HTTP_PROXY http_proxy HTTPS_PROXY https_proxy NO_PROXY no_proxy
