bdd104586c5e105129a4fc53b43e3c0a67b43482
yanggaofeng <yanggaofeng@sxxfuture.com>
2022-01-12 15:58:52 +0800

    merge

--- build/params_interop.go
+++ build/params_interop.go
@@ -14,7 +14,6 @@ import (
 	"github.com/filecoin-project/go-state-types/network"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	miner6 "github.com/filecoin-project/specs-actors/v6/actors/builtin/miner"
 )
 
 const BootstrappersFile = "interopnet.pi"
@@ -98,8 +97,6 @@ func init() {
 	SetAddressNetwork(address.Testnet)
 	Devnet = true
 
-	// To test out what this proposal would like on devnets / testnets: https://github.com/filecoin-project/FIPs/pull/190
-	miner6.FaultMaxAge = miner6.WPoStProvingPeriod * 42
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
