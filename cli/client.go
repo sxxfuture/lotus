3d0d664d2c728e3e90e205ec461756e501471b1d
francis_xiiiv <francis_xiiiv@163.com>
2022-01-26 22:27:43 -0800

    output raw piece size

--- cli/client.go
+++ cli/client.go
@@ -228,6 +228,7 @@ var clientCommPCmd = &cli.Command{
 
 		fmt.Println("CID: ", encoder.Encode(ret.Root))
 		fmt.Println("Piece size: ", types.SizeStr(types.NewInt(uint64(ret.Size))))
+		fmt.Println("Raw piece size: ", uint64(ret.Size))
 		return nil
 	},
 }
