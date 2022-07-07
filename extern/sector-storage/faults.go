9cf418910c931b5f90892d620b92807c9952299f
xiaohailin <xiaohailin@sxxfuture.com>
2022-07-07 13:28:46 +0800

    lotus-miner proving check --only-bad n 从v1.15后此命令无法使用。修改代码使其继续可用。

--- extern/sector-storage/faults.go
+++ extern/sector-storage/faults.go
@@ -4,19 +4,18 @@ import (
 	"context"
 	"crypto/rand"
 	"fmt"
-	"sync"
-	"time"
+	"os"
+	"path/filepath"
 
 	"golang.org/x/xerrors"
 
 	ffi "github.com/filecoin-project/filecoin-ffi"
 	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/specs-actors/actors/runtime/proof"
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-
-var PostCheckTimeout = 160 * time.Second
 
 // FaultTracker TODO: Track things more actively
 type FaultTracker interface {
@@ -25,110 +24,142 @@ type FaultTracker interface {
 
 // CheckProvable returns unprovable sectors
 func (m *Manager) CheckProvable(ctx context.Context, pp abi.RegisteredPoStProof, sectors []storage.SectorRef, rg storiface.RGetter) (map[abi.SectorID]string, error) {
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	if rg == nil {
-		return nil, xerrors.Errorf("rg is nil")
-	}
-
 	var bad = make(map[abi.SectorID]string)
-	var badLk sync.Mutex
 
-	var postRand abi.PoStRandomness = make([]byte, abi.RandomnessLength)
-	_, _ = rand.Read(postRand)
-	postRand[31] &= 0x3f
-
-	limit := m.parallelCheckLimit
-	if limit <= 0 {
-		limit = len(sectors)
-	}
-	throttle := make(chan struct{}, limit)
-
-	addBad := func(s abi.SectorID, reason string) {
-		badLk.Lock()
-		bad[s] = reason
-		badLk.Unlock()
+	ssize, err := pp.SectorSize()
+	if err != nil {
+		return nil, err
 	}
 
-	var wg sync.WaitGroup
-	wg.Add(len(sectors))
-
+	// TODO: More better checks
 	for _, sector := range sectors {
-		select {
-		case throttle <- struct{}{}:
-		case <-ctx.Done():
-			return nil, ctx.Err()
-		}
-
-		go func(sector storage.SectorRef) {
-			defer wg.Done()
-			defer func() {
-				<-throttle
-			}()
+		err := func() error {
 			ctx, cancel := context.WithCancel(ctx)
 			defer cancel()
+			var fReplica string
+			var fCache string
 
-			commr, update, err := rg(ctx, sector.ID)
+			locked, err := m.index.StorageTryLock(ctx, sector.ID, storiface.FTSealed|storiface.FTCache, storiface.FTNone)
 			if err != nil {
-				log.Warnw("CheckProvable Sector FAULT: getting commR", "sector", sector, "sealed", "err", err)
-				addBad(sector.ID, fmt.Sprintf("getting commR: %s", err))
-				return
-			}
-
-			toLock := storiface.FTSealed | storiface.FTCache
-			if update {
-				toLock = storiface.FTUpdate | storiface.FTUpdateCache
-			}
-
-			locked, err := m.index.StorageTryLock(ctx, sector.ID, toLock, storiface.FTNone)
-			if err != nil {
-				addBad(sector.ID, fmt.Sprintf("tryLock error: %s", err))
-				return
+				return xerrors.Errorf("acquiring sector lock: %w", err)
 			}
 
 			if !locked {
 				log.Warnw("CheckProvable Sector FAULT: can't acquire read lock", "sector", sector)
-				addBad(sector.ID, fmt.Sprint("can't acquire read lock"))
-				return
+				bad[sector.ID] = fmt.Sprint("can't acquire read lock")
+				return nil
 			}
 
-			wpp, err := sector.ProofType.RegisteredWindowPoStProof()
+			lp, _, err := m.localStore.AcquireSector(ctx, sector, storiface.FTSealed|storiface.FTCache, storiface.FTNone, storiface.PathStorage, storiface.AcquireMove)
 			if err != nil {
-				addBad(sector.ID, fmt.Sprint("can't get proof type"))
-				return
+				log.Warnw("CheckProvable Sector FAULT: acquire sector in checkProvable", "sector", sector, "error", err)
+				bad[sector.ID] = fmt.Sprintf("acquire sector failed: %s", err)
+				return nil
+			}
+			fReplica, fCache = lp.Sealed, lp.Cache
+
+			if fReplica == "" || fCache == "" {
+				log.Warnw("CheckProvable Sector FAULT: cache and/or sealed paths not found", "sector", sector, "sealed", fReplica, "cache", fCache)
+				bad[sector.ID] = fmt.Sprintf("cache and/or sealed paths not found, cache %q, sealed %q", fCache, fReplica)
+				return nil
 			}
 
-			ch, err := ffi.GeneratePoStFallbackSectorChallenges(wpp, sector.ID.Miner, postRand, []abi.SectorNumber{
-				sector.ID.Number,
-			})
-			if err != nil {
-				log.Warnw("CheckProvable Sector FAULT: generating challenges", "sector", sector, "err", err)
-				addBad(sector.ID, fmt.Sprintf("generating fallback challenges: %s", err))
-				return
+			toCheck := map[string]int64{
+				fReplica:                       1,
+				filepath.Join(fCache, "p_aux"): 0,
 			}
 
-			vctx, cancel2 := context.WithTimeout(ctx, PostCheckTimeout)
-			defer cancel2()
+			addCachePathsForSectorSize(toCheck, fCache, ssize)
 
-			_, err = m.storage.GenerateSingleVanillaProof(vctx, sector.ID.Miner, storiface.PostSectorChallenge{
-				SealProof:    sector.ProofType,
-				SectorNumber: sector.ID.Number,
-				SealedCID:    commr,
-				Challenge:    ch.Challenges[sector.ID.Number],
-				Update:       update,
-			}, wpp)
-			if err != nil {
-				log.Warnw("CheckProvable Sector FAULT: generating vanilla proof", "sector", sector, "err", err)
-				addBad(sector.ID, fmt.Sprintf("generating vanilla proof: %s", err))
-				return
+			for p, sz := range toCheck {
+				st, err := os.Stat(p)
+				if err != nil {
+					log.Warnw("CheckProvable Sector FAULT: sector file stat error", "sector", sector, "sealed", fReplica, "cache", fCache, "file", p, "err", err)
+					bad[sector.ID] = fmt.Sprintf("%s", err)
+					return nil
+				}
+
+				if sz != 0 {
+					if st.Size() != int64(ssize)*sz {
+						log.Warnw("CheckProvable Sector FAULT: sector file is wrong size", "sector", sector, "sealed", fReplica, "cache", fCache, "file", p, "size", st.Size(), "expectSize", int64(ssize)*sz)
+						bad[sector.ID] = fmt.Sprintf("%s is wrong size (got %d, expect %d)", p, st.Size(), int64(ssize)*sz)
+						return nil
+					}
+				}
 			}
-		}(sector)
+
+			if rg != nil {
+				wpp, err := sector.ProofType.RegisteredWindowPoStProof()
+				if err != nil {
+					return err
+				}
+
+				var pr abi.PoStRandomness = make([]byte, abi.RandomnessLength)
+				_, _ = rand.Read(pr)
+				pr[31] &= 0x3f
+
+				ch, err := ffi.GeneratePoStFallbackSectorChallenges(wpp, sector.ID.Miner, pr, []abi.SectorNumber{
+					sector.ID.Number,
+				})
+				if err != nil {
+					log.Warnw("CheckProvable Sector FAULT: generating challenges", "sector", sector, "sealed", fReplica, "cache", fCache, "err", err)
+					bad[sector.ID] = fmt.Sprintf("generating fallback challenges: %s", err)
+					return nil
+				}
+
+				commr, _, err := rg(ctx, sector.ID)
+				if err != nil {
+					log.Warnw("CheckProvable Sector FAULT: getting commR", "sector", sector, "sealed", fReplica, "cache", fCache, "err", err)
+					bad[sector.ID] = fmt.Sprintf("getting commR: %s", err)
+					return nil
+				}
+
+				_, err = ffi.GenerateSingleVanillaProof(ffi.PrivateSectorInfo{
+					SectorInfo: proof.SectorInfo{
+						SealProof:    sector.ProofType,
+						SectorNumber: sector.ID.Number,
+						SealedCID:    commr,
+					},
+					CacheDirPath:     fCache,
+					PoStProofType:    wpp,
+					SealedSectorPath: fReplica,
+				}, ch.Challenges[sector.ID.Number])
+				if err != nil {
+					log.Warnw("CheckProvable Sector FAULT: generating vanilla proof", "sector", sector, "sealed", fReplica, "cache", fCache, "err", err)
+					bad[sector.ID] = fmt.Sprintf("generating vanilla proof: %s", err)
+					return nil
+				}
+			}
+
+			return nil
+		}()
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	wg.Wait()
-
 	return bad, nil
+}
+
+func addCachePathsForSectorSize(chk map[string]int64, cacheDir string, ssize abi.SectorSize) {
+	switch ssize {
+	case 2 << 10:
+		fallthrough
+	case 8 << 20:
+		fallthrough
+	case 512 << 20:
+		chk[filepath.Join(cacheDir, "sc-02-data-tree-r-last.dat")] = 0
+	case 32 << 30:
+		for i := 0; i < 8; i++ {
+			chk[filepath.Join(cacheDir, fmt.Sprintf("sc-02-data-tree-r-last-%d.dat", i))] = 0
+		}
+	case 64 << 30:
+		for i := 0; i < 16; i++ {
+			chk[filepath.Join(cacheDir, fmt.Sprintf("sc-02-data-tree-r-last-%d.dat", i))] = 0
+		}
+	default:
+		log.Warnf("not checking cache files of %s sectors for faults", ssize)
+	}
 }
 
 var _ FaultTracker = &Manager{}
